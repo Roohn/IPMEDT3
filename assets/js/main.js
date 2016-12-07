3fd1fb49341e6a963e6ed552ba7a5a86a500f518
Ronald Tol <tol.ronald@gmail.com>
2016-12-07 12:53:43 +0100

    processording oppakken

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
 
+    $('#processor').on('click', function () {
+        // cursor klik animatie
+        cursorClick();
+        // model aan camera (oppakken)
+        showModelOnCamera(this.id);
+    });
 
 });
+
+function cursorClick() {
+    // Trigger de cursor om de klik animatie uit te voeren
+    document.querySelector('#cursor').emit('cursorclick');
+}
+
+function showModelOnCamera(model) {
+    $('#' + model).remove();
+    $('#' + model + '-camera').attr('visible', true);
+}
+
+function putModelInCase(model, plek) {
+
+    // TO DO
+    // stop model op de goede plek in de pc kast
+
+}
