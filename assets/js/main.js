e8598db39fc08e9cbc25ff0eba73370b1f87dd3f
Ronald Tol <tol.ronald@gmail.com>
2016-12-08 11:36:40 +0100

    pakken van stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
 $(document).ready(function () {
+    var stap = 0;
 
     $('#processor').on('click', function () {
         // cursor klik animatie
         cursorClick();
         // model aan camera (oppakken)
         showModelOnCamera(this.id);
-    });
 
+        //opgepakt
+        $('#page').append('<a-entity id="processor-indicator" position="2.85 4.55 -3.6">' +
+            '<a-plane class="indicator" height=".6" width=".7" color="yellow" rotation="-90 0 0"></a-plane>' +
+            '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="1500"></a-animation>' +
+            '</a-entity>');
+
+        $('#processor-indicator').on('click', function () {
+            //motherboard en processor vervangen door combi
+            $(this).remove();
+            $('#processor-camera').attr('visible', false);
+
+            //combi van moederboard en processor
+            $('#combi-processor').attr('visible', true);
+            stap = 1;
+        });
+    });
+    $('#motherboard').on('click', function () {
+        if(stap == 1){
+            cursorClick();
+            showModelOnCamera(this.id);
+        }
+    });
 });
 
 function cursorClick() {
@@ -21,7 +43,7 @@ function showModelOnCamera(model) {
 
 function putModelInCase(model, plek) {
 
-    // TO DO
     // stop model op de goede plek in de pc kast
+    console.log(model + " in " + plek);
 
 }
