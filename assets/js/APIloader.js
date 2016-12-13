9bd993509bf95b0edf2f26edc17f4b7891f5e03d
Ronald Tol <tol.ronald@gmail.com>
2016-12-13 15:09:42 +0100

    API van wikipedia toegevoegd die informatie geeft over het component dat je vasthoud

---
+++ assets/js/APIloader.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('#getInfo').on('click', function () {
+        if (stap == 0) {
+            getData('Processor_(computer)');
+        }
+        if (stap == 1) {
+            getData('Moederbord');
+        }
+        // TODO: alle andere stappen toevoegen
+    })
+});
+
+function getData(dataString){
+    $.ajax({
+        url: 'https://nl.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + dataString,
+        method: 'GET',
+        dataType: "jsonp",
+        success: function(msg) {
+            var pageID = 0;
+
+            switch (dataString) {
+                case "Moederbord":
+                    pageID = 11383;
+                    break;
+                case "Processor_(computer)":
+                    pageID = 1353;
+                    break;
+                case "Random-access_memory":
+                    pageID = 11427;
+                    break;
+                case "Voeding_(elektronica)":
+                    pageID = 142004;
+                    break;
+                case "Videokaart":
+                    pageID = 23292;
+                    break;
+            }
+            var informatie = (msg['query']['pages'][pageID]['extract']);
+            $('#informatie-text').attr('bmfont-text', 'text: ' + informatie + '; color: white; width: 800.00; lineHeight: 33.00');
+        }
+    });
+}
