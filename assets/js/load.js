c0571a2c1076b2ea0277c185583217e3725f1f2e
Ronald Tol <tol.ronald@gmail.com>
2016-12-21 11:21:40 +0100

    cursor bugfix

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$("#continueButton").on("click", function(){
-	    console.log("click");
 		$('#vrDiv').css('visibility', 'visible');
 		$('#splashDiv').css('display', 'none');
 	});
