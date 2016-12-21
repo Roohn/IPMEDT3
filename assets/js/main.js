23ddc656f20d439fc924d9f4e2fea657378214d7
Jim Mak <jim.greenday@gmail.com>
2016-12-21 11:10:42 +0100

    Goede flow! :D

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $('#tutorial-text').attr('bmfont-text',' ');
 
         //opgepakt
-        $('#page').append('<a-entity id="processor-indicator" position="2.85 4.55 -3.6">' +
+        $('#page').append('<a-entity id="processor-indicator" position="3.85 4.55 -3.6">' +
             '<a-plane class="indicator" height=".6" width=".7" color="yellow" rotation="-90 0 0"></a-plane>' +
             '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="1500"></a-animation>' +
             '</a-entity>');
@@ -30,9 +30,31 @@ $(document).ready(function () {
             stap = 1;
         });
     });
+	
+	$('#koelblok').on('click', function () {
+		if (stap == 1) {
+			cursorClick();
+			showModelOnCamera(this.id);
+			$('#page').append('<a-entity id="koelblok-indicator" position="3.85 4.65 -3.6">' +
+                '<a-plane class="indicator" height=".6" width=".7" color="yellow" rotation="-90 0 0"></a-plane>' +
+                '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="2000"></a-animation>' +
+                '</a-entity>');
+				
+			$("#koelblok-indicator").on("click", function() {
+				playCorrectSound();
+				
+				$(this).remove();
+				$("#koelblok-camera").remove();
+				
+				$("#combi-koelblok").attr("visible", true);
+				stap = 2;
+				
+			});
+		}
+	});
 
     $('#motherboard').on('click', function () {
-        if (stap == 1) {
+        if (stap == 2) {
             cursorClick();
             showModelOnCamera(this.id);
             $('#page').append('<a-entity id="motherboard-indicator" position="-3.3 6.46 -2.68">' +
@@ -48,11 +70,33 @@ $(document).ready(function () {
                 $('#motherboard-camera').remove();
                 $('#kast-motherboard').attr('visible', true);
                 $('#kast-processor').attr('visible', true);
+				$("#kast-koelblok").attr("visible", true);
 
-                stap = 2;
+                stap = 3;
             });
         }
     });
+	
+	$("#voeding").on("click", function() {
+		if (stap == 3) {
+			cursorClick();
+			showModelOnCamera(this.id);
+			$('#page').append('<a-entity id="voeding-indicator" position="-3.51 4.87 -2.45">' +
+                '<a-plane class="indicator" height="0.77" width="1.554" color="yellow" rotation="0 45 0"></a-plane>' +
+                '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="2000"></a-animation>' +
+                '</a-entity>');
+			$("#voeding-indicator").on("click", function() {
+				playCorrectSound();
+				$(this).remove();
+				$("#voeding-camera").remove();
+				$("#kast-voeding").attr("visible", true);
+				
+				stap = 4;
+			});
+		}
+	});
+	
+	
 });
 
 function cursorClick() {
