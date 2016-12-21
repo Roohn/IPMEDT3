76812ebcde32ef08ea3d8990e9551960271baa1b
Ronald Tol <tol.ronald@gmail.com>
2016-12-21 12:20:45 +0100

    git splash screen

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-	$("#continueButton").on("click", function(){
-		$('#vrDiv').css('visibility', 'visible');
-		$('#splashDiv').css('display', 'none');
-	});
+	$('#logo-gif').attr('src', 'assets/img/logo-gif.gif'); //reset gif animatie
+	setTimeout(loaded, 8000);
 });
+
+function loaded() {
+	$('#vrDiv').css('visibility', 'visible');
+	$('#splashDiv').css('display', 'none');
+}
