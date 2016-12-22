be4a0230b14c91ae1d701e17e1a7880095316d33
Ronald Tol <tol.ronald@gmail.com>
2016-12-22 08:46:22 +0100

    class='clickable' toevoegen als je het object kan aanklikken (goed voor performance)

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $('#tutorial-text').attr('bmfont-text',' ');
 
         //opgepakt
-        $('#page').append('<a-entity id="processor-indicator" position="3.85 4.55 -3.6">' +
+        $('#page').append('<a-entity class="clickable" id="processor-indicator" position="3.85 4.55 -3.6">' +
             '<a-plane class="indicator" height=".6" width=".7" color="yellow" rotation="-90 0 0"></a-plane>' +
             '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="1500"></a-animation>' +
             '</a-entity>');
@@ -35,7 +35,7 @@ $(document).ready(function () {
 		if (stap == 1) {
 			cursorClick();
 			showModelOnCamera(this.id);
-			$('#page').append('<a-entity id="koelblok-indicator" position="3.85 4.65 -3.6">' +
+			$('#page').append('<a-entity class="clickable" id="koelblok-indicator" position="3.85 4.65 -3.6">' +
                 '<a-plane class="indicator" height=".6" width=".7" color="yellow" rotation="-90 0 0"></a-plane>' +
                 '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="2000"></a-animation>' +
                 '</a-entity>');
@@ -57,7 +57,7 @@ $(document).ready(function () {
         if (stap == 2) {
             cursorClick();
             showModelOnCamera(this.id);
-            $('#page').append('<a-entity id="motherboard-indicator" position="-3.3 6.46 -2.68">' +
+            $('#page').append('<a-entity class="clickable" id="motherboard-indicator" position="-3.3 6.46 -2.68">' +
                 '<a-plane class="indicator" height="2.1" width="2.2" color="yellow" rotation="0 45 0"></a-plane>' +
                 '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="2000"></a-animation>' +
                 '</a-entity>');
@@ -81,7 +81,7 @@ $(document).ready(function () {
 		if (stap == 3) {
 			cursorClick();
 			showModelOnCamera(this.id);
-			$('#page').append('<a-entity id="voeding-indicator" position="-3.51 4.87 -2.45">' +
+			$('#page').append('<a-entity class="clickable" id="voeding-indicator" position="-3.51 4.87 -2.45">' +
                 '<a-plane class="indicator" height="0.77" width="1.554" color="yellow" rotation="0 45 0"></a-plane>' +
                 '<a-animation attribute="scale" from="1 1 1" to="0 0 0" repeat="indefinite" dur="2000"></a-animation>' +
                 '</a-entity>');
